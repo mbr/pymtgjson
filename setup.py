02710757930b3deb84c071604e4c0c54da2441aa
Marc Brinkmann <git@marcbrinkmann.de>
2014-12-25 17:17:15 +0100

    Increased version to 0.4.dev1 after release of 0.3.
    (commit by unleash 0.5.8.dev1)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='mtgjson',
-    version='0.3.dev1',
+    version='0.4.dev1',
     description='A python library for working with data from mtgjson.com.',
     long_description=read('README.rst'),
     author='Marc Brinkmann',
