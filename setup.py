e9afa805ed32efeaa8f685a995613e827bc8dea2
Marc Brinkmann <git@marcbrinkmann.de>
2015-12-09 09:21:45 +0100

    Version change and YaPF.

--- setup.py
+++ setup.py
@@ -10,15 +10,13 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-setup(
-    name='mtgjson',
-    version='0.4.dev1',
-    description='A python library for working with data from mtgjson.com.',
-    long_description=read('README.rst'),
-    author='Marc Brinkmann',
-    author_email='git@marcbrinkmann.de',
-    url='http://github.com/mbr/pymtgjson',
-    license='MIT',
-    packages=find_packages(exclude=['tests']),
-    install_requires=['docutils', 'requests', 'six'],
-)
+setup(name='mtgjson',
+      version='0.4.0.dev1',
+      description='A python library for working with data from mtgjson.com.',
+      long_description=read('README.rst'),
+      author='Marc Brinkmann',
+      author_email='git@marcbrinkmann.de',
+      url='http://github.com/mbr/pymtgjson',
+      license='MIT',
+      packages=find_packages(exclude=['tests']),
+      install_requires=['docutils', 'requests', 'six'], )
