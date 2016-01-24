56ea8f25fd2dbc2b48a8188d46cf43b0ffe62d34
Michael Milton <ttmigueltt@gmail.com>
2016-01-24 21:46:37 +0100

    Implemented dictionary operators for jsonproxy

--- mtgjson/jsonproxy.py
+++ mtgjson/jsonproxy.py
@@ -2,6 +2,18 @@ class JSONProxy(object):
     def __init__(self, data):
         self.__data = data
 
+    def  __iter__(self):
+        return self.__data.__iter__()
+
+    def __getitem__(self, key):
+        try:
+            return self.__data[key]
+        except KeyError as e:
+            raise AttributeError(e)
+
+    def __contains__(self, item):
+        return self.__data.__contains__(item)
+
     def __getattr__(self, key):
         try:
             return self.__data[key]
@@ -10,3 +22,4 @@ class JSONProxy(object):
 
     def _get_raw_data(self):
         return self.__data
+
